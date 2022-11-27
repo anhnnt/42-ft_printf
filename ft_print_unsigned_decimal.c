791ddea8979ced502e93adddbb0ceb0a4030f2e3
Anh Nguyen <33486007+anhnnt@users.noreply.github.com>
2022-11-27 13:48:00 +0200

    update uint

--- ft_print_unsigned_decimal.c
+++ ft_print_unsigned_decimal.c
@@ -12,11 +12,26 @@
 
 #include "includes/ft_printf.h"
 
-int	ft_print_unsigned_decimal(unsigned int i)
+void	ft_putnbr_uint(unsigned int n)
 {
-	size_t	num;
+	if (n >= 0 && n < 10)
+	{
+		ft_putchar_fd(n + 48, 1);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, 1);
+		ft_putnbr_fd(n % 10, 1);
+	}
+}
 
-	num = int_leng(i);
-	ft_putnbr_fd(i, 1);
-	return (num);
+int	ft_print_unsigned_decimal(long long n)
+{
+	unsigned int	num;
+	int				leng;
+
+	num = (unsigned int)(n);
+	leng = int_leng(num);
+	ft_putnbr_uint(num);
+	return (leng);
 }
